a3396fc0d240f8e8c79ea1e3859544a676e51b18
NOGY <norven.norov@gmail.com>
2021-02-06 00:39:31 +0800

    Updates

---
+++ precache-manifest.f3cb4d8234239198783d9d9299aeff87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1af179fb77c0915e831789f77b18d42",
+    "url": "/burgerdy/index.html"
+  },
+  {
+    "revision": "a64ddf1dd86e8a9b8229",
+    "url": "/burgerdy/static/css/main.77e9bfe1.chunk.css"
+  },
+  {
+    "revision": "2862fdaa4fcc12e107da",
+    "url": "/burgerdy/static/js/2.1fb0107f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerdy/static/js/2.1fb0107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64ddf1dd86e8a9b8229",
+    "url": "/burgerdy/static/js/main.5b137e94.chunk.js"
+  },
+  {
+    "revision": "72138a5524f68651fa0c",
+    "url": "/burgerdy/static/js/runtime-main.fb9a14b7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerdy/static/media/burger-logo.b8503d26.png"
+  }
+]);
